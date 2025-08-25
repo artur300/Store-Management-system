25ab4cad869332ce377e06aea966d54ff10d1ce6
rroyd <92038509+rroyd@users.noreply.github.com>
2025-08-25 20:04:11 +0300

    Continue logic and started the issues i'm assigned to

--- src/com/myshopnet/service/StockService.java
+++ src/com/myshopnet/service/StockService.java
@@ -1,19 +1,44 @@
 package com.myshopnet.service;
+import com.myshopnet.errors.EntityNotFoundException;
 import com.myshopnet.models.Branch;
 import com.myshopnet.models.Product;
 import com.myshopnet.repository.BranchRepository;
 import com.myshopnet.repository.ProductRepository;
 
+import java.util.List;
+
 public class StockService {
     private BranchRepository branchRepository = new BranchRepository();
     private ProductRepository productRepository = new ProductRepository();
-
 
     public void updateProductStock(String branchId, String productId, Long quantity) {
         Branch branch = branchRepository.get(branchId);
         Product product = productRepository.get(productId);
 
+        if (product == null) {
+            throw new EntityNotFoundException("Product");
+        }
 
+        if (branch == null) {
+            throw new EntityNotFoundException("Branch");
+        }
+
+        branch.getProductsStock().getStockOfProducts().replace(product, quantity);
+    }
+
+    public void removeProductStockFromBranch(String productId) {
+        List<Branch> allBranch = branchRepository.getAll();
+        Product product = productRepository.get(productId);
+
+        if (product == null) {
+            throw new EntityNotFoundException("Product");
+        }
+
+        for (Branch branch : allBranch) {
+            branch.getProductsStock().getStockOfProducts().remove(productId);
+
+            branchRepository.update(branch.getId(), branch);
+        }
     }
 }
 
